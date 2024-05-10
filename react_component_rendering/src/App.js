1c10f09d09cbee47545fefd29f67966466098dda
Nishan Ahmed <nishan@Nishans-MacBook-Air.local>
2024-05-10 21:41:33 +0600

    Parent child render

--- react_component_rendering/src/App.js
+++ react_component_rendering/src/App.js
@@ -2,18 +2,21 @@ import './App.css';
 import UseState from './components/useState/UseState'
 import UseReducer from './components/useReducer/UseReducer'
 import ObjectUseState from './components/ImmutableState/ObjectUseState'
+import Parent from './components/ParentChildRender/Parent';
 
 function App() {
-  return (
+return (
     <div className="App">
-      <h2>UseState Render</h2>
-      <UseState />
-      <h2>UseReduce Render</h2>
-      <UseReducer />
-      <h2>Object useState Render</h2>
-      <ObjectUseState />
+        <h2>UseState Render</h2>
+        <UseState />
+        <h2>UseReduce Render</h2>
+        <UseReducer />
+        <h2>Object useState Render</h2>
+        <ObjectUseState />
+        <h2>Parent Child Render</h2>
+        <Parent />
     </div>
-  );
+);
 }
 
 export default App;
---
+++ react_component_rendering/src/components/ParentChildRender/Child.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Child = () => {
+    console.log('Child Render')
+
+    return (
+        <div>
+            Child component
+        </div>
+    )
+}
+
+export default Child
