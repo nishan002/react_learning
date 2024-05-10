87416ecf51fed4ea8bdacad131983f110c6dd1c6
Nishan Ahmed <nishan@Nishans-MacBook-Air.local>
2024-05-10 20:25:06 +0600

    component render for useReducer

--- react_component_rendering/src/App.js
+++ react_component_rendering/src/App.js
@@ -1,11 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 import UseState from './components/useState/UseState'
+import UseReducer from './components/useReducer/UseReducer'
 
 function App() {
   return (
     <div className="App">
+      <h2>UseState Render</h2>
       <UseState />
+      <h2>UseReduce Render</h2>
+      <UseReducer />
     </div>
   );
 }
